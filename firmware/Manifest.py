3c22de5e20488466e27b14bcbb1b2b0340ff20b7
Cedric Viou <cedric.viou@obs-nancay.fr>
2022-03-04 15:47:23 +0100

    Add ohwr.org/project/general-cores

--- firmware/Manifest.py
+++ firmware/Manifest.py
@@ -7,9 +7,9 @@ modules = {
   "git" : [
     "https://ohwr.org/project/fpga-config-space.git",
     "https://ohwr.org/project/etherbone-core.git",
-    #"https://ohwr.org/project/general-cores.git",
+    "https://ohwr.org/project/general-cores.git",
     "https://ohwr.org/project/wr-cores.git",
-    "https://ohwr.org/project/wrpc-sw.git",
+    #"https://ohwr.org/project/wrpc-sw.git",
     #"https://ohwr.org/project/hdl-make.git",
   ],
 }
