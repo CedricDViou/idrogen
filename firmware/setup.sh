44c786848e28d18d4d7431a414f3fe7c660ebf50
Antoine BACK <antoine.back@ijclab.in2p3.fr>
2022-07-25 12:20:53 +0000

    Add script to setup project

---
+++ firmware/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git submodule init
+git submodule update
+
+TMP="ip_cores/general-cores/modules/common/tmp.py"
+FILE="ip_cores/general-cores/modules/common/Manifest.py"
+
+if grep -q "matrix_pkg.vhd" "$FILE"; then
+    echo "Manifest already contains matrix_pkg.vhd file."
+else
+    echo "Missing matrix_pkg.vhd file in Manifest."
+    cp "$FILE" "$TMP"
+    sed '3i\\t"matrix_pkg.vhd",' "$TMP" > "$FILE"
+    rm "$TMP"
+fi
