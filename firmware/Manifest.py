85eed9d79f5a16864b25b707aad03af0c039352e
Antoine BACK <antoine.back@ijclab.in2p3.fr>
2022-09-06 07:20:11 +0000

    Add pcie module

--- firmware/Manifest.py
+++ firmware/Manifest.py
@@ -1,15 +1,16 @@
 fetchto = "ip_cores"
 
 modules = {
-  "local" : [
-    "modules",
-  ],
-  "git" : [
-    #"https://ohwr.org/project/fpga-config-space.git",
-    "https://ohwr.org/project/etherbone-core.git",
-    "https://ohwr.org/project/general-cores.git",
-    "https://ohwr.org/project/wr-cores.git",
-    #"https://ohwr.org/project/wrpc-sw.git",
-    #"https://ohwr.org/project/hdl-make.git",
-  ],
+    "local": [
+        "modules",
+    ],
+    "git": [
+        # "https://ohwr.org/project/fpga-config-space.git",
+        "https://ohwr.org/project/etherbone-core.git",
+        "https://ohwr.org/project/general-cores.git",
+        "https://ohwr.org/project/wr-cores.git",
+        "https://gitlab.in2p3.fr/antoine.back/firmware-pcie.git",
+        # "https://ohwr.org/project/wrpc-sw.git",
+        # "https://ohwr.org/project/hdl-make.git",
+    ],
 }
