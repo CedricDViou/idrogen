1648dd5a2b3dd4da3988339897ae8fdfd0e2c784
Antoine BACK <antoine.back@ijclab.in2p3.fr>
2022-10-26 12:35:24 +0000

    Remove pll_pkg.vhd File already in Ipbus-1G repository

--- firmware/modules/pll/Manifest.py
+++ firmware/modules/pll/Manifest.py
@@ -1,13 +1,15 @@
 def __helper():
-  dirs = []
-  if syn_device[:9] == "10AX027H4":      dirs.extend(["arria10"])
-  return dirs
+    dirs = []
+    if syn_device[:9] == "10AX027H4":
+        dirs.extend(["arria10"])
+    return dirs
+
 
 files = [
-  "pll_pkg.vhd",
-  "altera_butis.vhd",
-  "altera_phase.vhd",
-  "altera_reset.vhd" ]
-  
-modules = {"local": __helper() }
+    # "pll_pkg.vhd",
+    "altera_butis.vhd",
+    "altera_phase.vhd",
+    "altera_reset.vhd"
+]
 
+modules = {"local": __helper()}
