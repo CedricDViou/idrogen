280e6353a833f48df226f266b66066a3bbe225f2
Antoine BACK <antoine.back@ijclab.in2p3.fr>
2022-11-04 10:23:06 +0000

    Check fifo status

--- firmware/modules/ipbus-1G/wr_monitor.v
+++ firmware/modules/ipbus-1G/wr_monitor.v
@@ -6,21 +6,23 @@ module  wr_monitor(
     output reg          bridge_uart_read        = 0,
     output reg          bridge_uart_write       = 0,
     output reg  [ 3:0]  bridge_uart_byte_enable = 0,
-    output reg  [ 5:0]  bridge_uart_address     = 0,
+    output reg  [ 8:0]  bridge_uart_address     = 0,
     output reg  [31:0]  bridge_uart_write_data  = 0
 );
 
-    localparam FIFO_ADDRESS         = 6'h10;
-    localparam UART_ADDRESS_READ    = 6'h20;
-    localparam UART_ADDRESS_STATUS  = 6'h28;
+    localparam FIFO_WR_REG          = 9'h100;
+    localparam FIFO_STATUS_REG      = 9'h144;
+    localparam UART_ADDRESS_READ    = 9'h20;
+    localparam UART_ADDRESS_STATUS  = 9'h28;
 
     localparam WAIT_READ   = 0;
     localparam READ_UART   = 1;
-    localparam WRITE_FIFO  = 2;
-    localparam RESET_ERROR = 4;
+    localparam FIFO_STATUS = 2;
+    localparam WRITE_FIFO  = 4;
+    localparam RESET_ERROR = 8;
 
     reg [1:0] fifo_shift   = 0;
-    reg [2:0] reg_fstate   = 0;
+    reg [3:0] reg_fstate   = 0;
 
     always @(posedge clock or negedge nreset)
         begin : state_machine
@@ -65,7 +67,7 @@ module  wr_monitor(
                             bridge_uart_write_data  <= {bridge_uart_write_data[23:0], bridge_uart_read_data[7:0]};
 
                             if (fifo_shift==3)
-                                reg_fstate          <= WRITE_FIFO;
+                                reg_fstate          <= FIFO_STATUS;
                             else
                                 reg_fstate          <= WAIT_READ;
                         end
@@ -75,10 +77,28 @@ module  wr_monitor(
 
                         
                     end
+                    FIFO_STATUS: begin
+                        bridge_uart_read        <= 1;
+                        bridge_uart_byte_enable <= 1;
+                        bridge_uart_address     <= FIFO_STATUS_REG;
+
+                        if (bridge_uart_acknowledge) begin
+                            bridge_uart_read        <= 0;
+                            bridge_uart_byte_enable <= 0;
+
+                            if (bridge_uart_read_data[0])
+                                reg_fstate          <= WAIT_READ;
+                            else
+                                reg_fstate          <= WRITE_FIFO;
+                        end
+                        else begin
+                            reg_fstate <= FIFO_STATUS;
+                        end
+                    end
                     WRITE_FIFO: begin
                         bridge_uart_write       <= 1;
                         bridge_uart_byte_enable <= 4'hF;
-                        bridge_uart_address     <= FIFO_ADDRESS;
+                        bridge_uart_address     <= FIFO_WR_REG;
 
                         if (bridge_uart_acknowledge) begin
                             reg_fstate              <= WAIT_READ;
@@ -105,8 +125,8 @@ module  wr_monitor(
                             reg_fstate <= RESET_ERROR;
                         end
                     end
-                    'd8: begin
-
+                    default: begin
+                        reg_fstate <= WAIT_READ;
                     end
                 endcase
             end
